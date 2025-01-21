38a5911f1a720c30c8f4656ee3b845ff341b63f7
eperniola <eperniola@litebox.ai>
2025-01-21 17:46:27 -0300

    feat: added desktop view of terms and conditions

--- apps/front/app/terms-and-conditions/page.tsx
+++ apps/front/app/terms-and-conditions/page.tsx
@@ -1,9 +1,10 @@
 import TermsMobile from '@/components/termsAndConditions/mobile/main';
-// import TermsDesktop from '@/components/termsAndConditions/desktop/main';
+import TermsDesktop from '@/components/termsAndConditions/desktop/main';
 
 export default function Home() {
   return (
     <main>
+      <TermsDesktop />
       <TermsMobile />
     </main>
   );
