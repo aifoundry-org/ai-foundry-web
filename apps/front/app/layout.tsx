9b0a8c95aa83724461b37057d8aa6b4c6a3bd928
Leila Donamaria <ldonamaria@litebox.ai>
2025-01-29 17:51:33 -0300

    feat: WIP Adding favicon

--- apps/front/app/layout.tsx
+++ apps/front/app/layout.tsx
@@ -1,6 +1,7 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import type { Metadata } from 'next';
+import './globals.css';
 
+import Head from 'next/head';
 import HeaderDesktop from '@/components/common/desktop/Header';
 import HeaderMobile from '@/components/common/mobile/Header';
 
@@ -8,8 +9,8 @@ import FooterMobile from '@/components/common/mobile/Footer';
 import FooterDesktop from '@/components/common/desktop/Footer';
 
 export const metadata: Metadata = {
-  title: "AI Foundry",
-  description: "An Open AI community for all",
+  title: 'AI Foundry',
+  description: 'An Open AI community for all',
 };
 
 export default function RootLayout({
@@ -18,7 +19,12 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang='en'>
+      <Head>
+        <link rel='icon' href='/favicon.ico' type='image/x-icon' />
+        <link rel='apple-touch-icon' href='/favicon-180x180.png' sizes='180x180' />
+        <link rel='icon' href='/favicon.png' type='image/png' sizes='60x60' />
+      </Head>
       <body>
         <HeaderDesktop />
         <HeaderMobile />
