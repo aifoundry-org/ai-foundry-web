9d6d8028f1744cac70db93be33ad6cde41273fae
eperniola <eperniola@litebox.ai>
2025-01-29 13:28:55 -0300

    feat: added marquee lottie

--- apps/front/components/home/desktop/OurCompanyDivider.tsx
+++ apps/front/components/home/desktop/OurCompanyDivider.tsx
@@ -1,9 +1,19 @@
-import IMGBackground from '@/public/pngs/home/ourCompanyDivider/background.png'
+'use client'
+
+import * as animationData from '@/data/home/lotties/marquee.json'
+import { useLottie } from 'lottie-react'
 
 export default function OurCompanyDivider(){
+    const defaultOptions = {
+        animationData: animationData,
+        loop: true,
+    };
+    
+    const { View } = useLottie(defaultOptions);
+
     return (
         <div className='hidden xs:flex xs:flex-col xs:mt-[3.5vw] xs:w-full xs:h-fit'>
-            <img src={IMGBackground.src} width="100%" alt="Our Company Divider" />
+            {View}
         </div>
     )
 }
--- apps/front/components/home/mobile/OurCompanyDivider.tsx
+++ apps/front/components/home/mobile/OurCompanyDivider.tsx
@@ -1,9 +1,19 @@
-import IMGBackground from '@/public/pngs/home/ourCompanyDivider/backgroundMobile.png'
+'use client'
+
+import * as animationData from '@/data/home/lotties/marquee.json'
+import { useLottie } from 'lottie-react'
 
 export default function OurCompanyDivider(){
+    const defaultOptions = {
+        animationData: animationData,
+        loop: true,
+    };
+    
+    const { View } = useLottie(defaultOptions);
+
     return (
         <div className='xs:hidden flex flex-col relative w-full h-fit mt-[13.7vw]'>
-            <img src={IMGBackground.src} width="100%" alt="Our Company Divider" />
+            {View}
         </div>
     )
 }
