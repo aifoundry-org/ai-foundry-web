7d120500dedceac222f9419ae91ec020f668d300
eperniola <eperniola@litebox.ai>
2025-01-29 13:24:59 -0300

    fix: vision mission animation

--- apps/front/components/home/desktop/VisionMission.tsx
+++ apps/front/components/home/desktop/VisionMission.tsx
@@ -22,19 +22,19 @@ export default function VisionMission(){
                         <img src={IMGErrors.src} width="100%" alt="Code Errors" />
                     </FadeIn>
                 </div>
-                <div className='w-full xs:pt-[35vw] md:pt-0 xl:pt-0 2xl:pt-0 flex flex-col basis-5/12 mx-[2vw]'>
-                    <FadeUp delay={1} className='mb-[2.25vw] uppercase font-black font-dharma-gothic-e text-[5.5vw] md:text-[2.8vw] xl:text-[2.8vw] 2xl:text-[2.8vw] leading-[4.5vw] md:leading-[2.3vw] xl:leading-[2.3vw] 2xl:leading-[2.3vw]'>
+                <FadeUp delay={2} className='w-full xs:pt-[35vw] md:pt-0 xl:pt-0 2xl:pt-0 flex flex-col basis-5/12 mx-[2vw]'>
+                    <div className='mb-[2.25vw] uppercase font-black font-dharma-gothic-e text-[5.5vw] md:text-[2.8vw] xl:text-[2.8vw] 2xl:text-[2.8vw] leading-[4.5vw] md:leading-[2.3vw] xl:leading-[2.3vw] 2xl:leading-[2.3vw]'>
                         Our long-term vision is to help the industry evolve to a structure where model training and deployment for any company are open, transparent, and concise.
-                    </FadeUp>
-                    <FadeUp delay={1.5} className='font-normal font-host-grotesk text-[3.5vw] md:text-[1.125vw] xl:text-[1.125vw] 2xl:text-[1.125vw] leading-[4vw] md:leading-[1.65vw] xl:leading-[1.65vw] 2xl:leading-[1.65vw]'>
+                    </div>
+                    <div className='font-normal font-host-grotesk text-[3.5vw] md:text-[1.125vw] xl:text-[1.125vw] 2xl:text-[1.125vw] leading-[4vw] md:leading-[1.65vw] xl:leading-[1.65vw] 2xl:leading-[1.65vw]'>
                         We desire to see the industry aligned on the principles of how those systems are structured at a high level from which every company can deploy its own engine or system, configured the way each specific company needs it. This approach will lower risks and improve AI results for all companies and practitioners. Therefore, we foster, build, and support projects that can become the engineering foundation of such training and deployment systems. We support practitioners willing to benefit from AI in their work and life, specifically models and their fine-tuning and deployment.
                         <br/><br/>
                         We are a community of practitioners building an open-source composable AI ecosystem. By collaborating on standards for everyone, we aim to reduce the complexity of the AI industry. 
-                    </FadeUp>
-                    <FadeUp delay={2} className='w-full h-fit mt-[2.25vw]'>
+                    </div>
+                    <div className='w-full h-fit mt-[2.25vw]'>
                         <Button as={Link} href='/terms-and-conditions' variant='secondary' content='Community code of conduct' svg='arrow-up-right' />
-                    </FadeUp>
-                </div>
+                    </div>
+                </FadeUp>
             </div>
             <div id='values' className='hidden md:flex md:flex-col md:relative md:w-full md:h-fit md:px-[8vw] md:pt-[14.2vw]'>
                 <FadeIn delay={1} className='w-full font-black text-[4.31vw] leading-[3.7vw] mb-[3.06vw] uppercase'>
