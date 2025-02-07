bf379c95870fd5e638d9bf695716485b78765b75
Leila Donamaria <ldonamaria@litebox.ai>
2025-02-07 12:02:28 -0300

    feat: WIP Adding strapi

---
+++ apps/strapi/src/api/community-post/services/community-post.ts
@@ -0,0 +1,7 @@
+/**
+ * community-post service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::community-post.community-post');
---
+++ apps/strapi/src/api/our-agentic-ai/services/our-agentic-ai.ts
@@ -0,0 +1,7 @@
+/**
+ * our-agentic-ai service
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreService('api::our-agentic-ai.our-agentic-ai');
