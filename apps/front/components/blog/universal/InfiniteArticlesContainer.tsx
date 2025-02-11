b93fea0ea7a8969e989ae07b3d2f2ed3d20077b9
eperniola <eperniola@litebox.ai>
2025-02-11 18:48:23 -0300

    fix: restored deleted functions

--- apps/front/components/blog/universal/InfiniteArticlesContainer.tsx
+++ apps/front/components/blog/universal/InfiniteArticlesContainer.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useMemo } from 'react';
+import { getArticles } from '@/backend/blog/actions';
 import { InfiniteArticlesContainerProps } from './InfiniteArticlesContainerType';
 import Spinner from '@/components/common/universal/Spinner';
 
@@ -11,6 +12,20 @@ const InfiniteArticlesContainer = ({
 }: InfiniteArticlesContainerProps) => {
     const [loading, setLoading] = useState(false);
     const [articles, setArticles] = useState(initArticles.data);
+    const hasMoreArticles = useMemo(() => {
+        const { start, limit, total } = initArticles.meta.pagination;
+        return (start+limit) < total
+    }, [articles]);
+
+    const showMoreArticles = async () => {
+        const offset = articles.length;
+        const newArticles = await getArticles(search, tags, offset);
+
+        setArticles(prevArticles => {
+            const uniqueArticles = newArticles.data.filter(newArticle => !prevArticles.some(prevArticle => prevArticle.id === newArticle.id));
+            return [...prevArticles, ...uniqueArticles];
+        });
+    };
 
     const sortedArticles = useMemo(() => {
         if(featuredArticleId){
@@ -34,6 +49,8 @@ const InfiniteArticlesContainer = ({
             <Spinner /> : 
             <InfiniteArticleComponent 
                 sortedArticles={sortedArticles}
+                hasMoreArticle={hasMoreArticles}
+                showMoreArticles={showMoreArticles}
                 featuredArticleId={featuredArticleId} 
             />
     );
