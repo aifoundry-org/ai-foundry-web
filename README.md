b76ac71edfb32c82abeabac94af39b57072919a5
Erasmo Perniola <eperniola@litebox.ai>
2025-03-07 18:44:41 +0000

    fix: added try catch when strapi api is unreachable

--- README.md
+++ README.md
@@ -7,6 +7,21 @@ This README provides instructions on how to set up and run the marketing site.
 To run the marketing site, follow these steps:
 
 1. Populate the `.env` file with the correct values based on the `.env.example` file.
-2. Navigate to the marketing site directory with: `cd apps/front`.
-3. Install the necessary dependencies with: `yarn install`.
-4. Start the development server with: `yarn dev`.
+2. Modify the `next.config.ts` file as shown below:
+```
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+    images: {
+        domains: ["<strapi_url_domain>"],
+    },
+};
+
+export default nextConfig;
+
+```
+to enable the images to be loaded correctly from strapi. The url <strapi_url_domain> does not
+need the https:// part (i.e. "strapi.ai-foundry.litebox.dev").
+3. Navigate to the marketing site directory with: `cd apps/front`.
+4. Install the necessary dependencies with: `yarn install`.
+5. Start the development server with: `yarn dev`.
--- apps/front/backend/blog/actions.ts
+++ apps/front/backend/blog/actions.ts
@@ -1,34 +1,79 @@
-import { StrapiArticle, StrapiTag, StrapiBlogData } from '@/libs/litebox-lib/types/strapi/strapiBlog';
+import { StrapiArticle, StrapiTag, StrapiBlogData, StrapiSeoComponent, StrapiParagraph, StrapiContentNavigation, StrapiAuthor } from '@/libs/litebox-lib/types/strapi/strapiBlog';
 import { fetchStrapi } from '@/libs/litebox-lib/utils/fetchStrapi';
 import { getArticlesQueryParams, getArticleQueryParams, getBlogFeaturedArticleQueryParams, getArticlesExcludingSlugQueryParams } from './queries'
+import { StrapiMedia } from '@/libs/litebox-lib/types/strapi/strapi';
 
 export const getArticles = async (search?: string, tags?: string[], offset = 0) => {
-    const articlesQueryParams = getArticlesQueryParams(search, tags, offset);
-    const articles = await fetchStrapi<StrapiArticle[]>(`/articles?${articlesQueryParams}`);
-    return articles;
+    try{
+        const articlesQueryParams = getArticlesQueryParams(search, tags, offset);
+        const articles = await fetchStrapi<StrapiArticle[]>(`/articles?${articlesQueryParams}`);
+        return articles;
+    } catch(error){
+        return {
+            data: [] as StrapiArticle[],
+            meta: {
+                pagination: {
+                    start: 0,
+                    limit: 0,
+                    total: 0
+                }
+            }
+        };
+    }
 };
 
 export const getLastArticles = async (limit = 3) => {
-    const articlesQueryParams = getArticlesQueryParams(undefined, undefined, 0, limit);
-    const articles = await fetchStrapi<StrapiArticle[]>(`/articles?${articlesQueryParams}`);
-    return articles;
+    try{
+        const articlesQueryParams = getArticlesQueryParams(undefined, undefined, 0, limit);
+        const articles = await fetchStrapi<StrapiArticle[]>(`/articles?${articlesQueryParams}`);
+        return articles;
+    } catch(error){
+        return {
+            data: [] as StrapiArticle[]
+        };
+    }
 };
 
 export const getArticle = async (slug: string) => {
-    const articleQueryParams = getArticleQueryParams(slug);
-    const article = await fetchStrapi<StrapiArticle[]>(`/articles?${articleQueryParams}`);
-    const relatedArticlesQueryParams = getArticlesExcludingSlugQueryParams(slug, 3);
-    const relatedArticles = await fetchStrapi<StrapiArticle[]>(`/articles?${relatedArticlesQueryParams}`);
-    const output = {
-        ...article.data[0], 
-        relatedArticles: relatedArticles.data
-    };
-    return output
+    try{
+        const articleQueryParams = getArticleQueryParams(slug);
+        const article = await fetchStrapi<StrapiArticle[]>(`/articles?${articleQueryParams}`);
+        const relatedArticlesQueryParams = getArticlesExcludingSlugQueryParams(slug, 3);
+        const relatedArticles = await fetchStrapi<StrapiArticle[]>(`/articles?${relatedArticlesQueryParams}`);
+        const output = {
+            ...article?.data?.[0], 
+            relatedArticles: relatedArticles.data
+        };
+        return output
+    } catch(error){
+        return {
+            id: 0,
+            slug: '',
+            title: '',
+            coverImage: {} as StrapiMedia,
+            seo: {} as StrapiSeoComponent,
+            authors: [] as StrapiAuthor[],
+            date: '',
+            paragraphs: [] as StrapiParagraph[],
+            contentNavigation: [] as StrapiContentNavigation[],
+            tags: [] as StrapiTag[],
+            relatedArticles: [] as StrapiArticle[],
+            createdAt: '',
+            publishedAt: '',
+            updatedAt: ''
+        }
+    }
 };
 
 export const getTags = async () => {
-    const strapiTags = await fetchStrapi<StrapiTag[]>(`/tags`);
-    return strapiTags;
+    try{
+        const strapiTags = await fetchStrapi<StrapiTag[]>(`/tags`);
+        return strapiTags;
+    } catch(error) {
+        return {
+            data: [] as StrapiTag[]
+        };
+    }
 };
 
 export const getBlogFeaturedArticle = async () => {
