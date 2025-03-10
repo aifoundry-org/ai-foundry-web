5ec26420f69e6516059b7372f03f6e65e120ac64
Roman Shaposhnik <rvs@apache.org>
2025-03-10 15:45:49 -0700

    Changing FOSDEM banner

--- apps/front/components/common/mobile/Announcement.tsx
+++ apps/front/components/common/mobile/Announcement.tsx
@@ -11,8 +11,8 @@ export default function Header() {
     
     return (
         <div className={`${hideSelf && 'hidden'} flex flex-row text-sand justify-center p-4`}>
-            <Link className='flex flex-col font-host-grotesk text-center items-center my-[1.35vw] text-[1.4rem]' target="_blank" href="https://aifoundry.org/fosdem-2025-low-level-ai-engineering-hacking-dev-room">
-                <div className='flex'>AIFoundry.org is sponsoring the &quot;Low-Level AI Engineering & Hacking&quot; Dev Room at FOSDEM. </div>
+            <Link className='flex flex-col font-host-grotesk text-center items-center my-[1.35vw] text-[1.4rem]' target="_blank" href="https://fosdem.org/2025/schedule/track/ai/">
+                <div className='flex'>AIFoundry.org hosted &quot;Low-Level AI Engineering & Hacking&quot; Dev Room at FOSDEM. Click to watch the talks!</div>
                 <div className='flex items-center'>Find out more! 
                     <div className='inline-block ml-[2.15vw] w-[3.735vw]'>
                         <ImageWrapper src={IMGHeaderIconArrowUpRight.src} alt="Arrow up right" />
