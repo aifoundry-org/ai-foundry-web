abaec24b5abc85851b2e5a701e0805ecb42ef9e0
Roman Shaposhnik <roman@nekko.ai>
2025-11-04 15:07:11 -0800

    Updating banner for FOSDEM 2026

--- apps/front/components/common/desktop/Announcement.tsx
+++ apps/front/components/common/desktop/Announcement.tsx
@@ -12,7 +12,7 @@ export default function Header() {
     return (
         <div className={`${hideSelf && 'hidden'} flex flex-row text-sand justify-center p-4`}>
             <Link target="_blank" className='flex font-host-grotesk text-[1.6rem]' href="https://fosdem.org/2025/schedule/track/ai/">
-                <div className='flex'>AIFoundry.org hosted &quot;Low-Level AI Engineering & Hacking&quot; Dev Room at FOSDEM. Click to watch the talks!</div>
+                <div className='flex'>AI Plumbers Devroom has been accepted for FOSDEM 2026! Submit your talk now before November 31st!</div>
                 <div className='flex items-center'>
                     <div className='inline-block ml-[0.56vw] w-[1.53vw]'>
                         <ImageWrapper src={IMGHeaderIconArrowUpRight.src} alt="Arrow up right" />
--- apps/front/components/common/mobile/Announcement.tsx
+++ apps/front/components/common/mobile/Announcement.tsx
@@ -10,7 +10,7 @@ export default function Header() {
     return (
         <div className={`${hideSelf && 'hidden'} flex flex-row text-sand justify-center p-4`}>
             <Link className='flex flex-col font-host-grotesk text-center items-center my-[1.35vw] text-[1.4rem]' target="_blank" href="https://fosdem.org/2025/schedule/track/ai/">
-                <div className='flex'>AIFoundry.org hosted &quot;Low-Level AI Engineering & Hacking&quot; Dev Room at FOSDEM. Click to watch the talks!</div>
+                <div className='flex'>AI Plumbers Devroom has been accepted for FOSDEM 2026! Submit your talk now before November 31st!</div>
             </Link>
         </div>
     )
