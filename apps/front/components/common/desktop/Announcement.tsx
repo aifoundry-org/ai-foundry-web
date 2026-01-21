385adb606896fb1ed6c59da09c32b83290290c3f
Eric Hallahan <eric@hallahans.name>
2026-01-21 13:06:41 -0500

    Update the announcement banner

--- apps/front/components/common/desktop/Announcement.tsx
+++ apps/front/components/common/desktop/Announcement.tsx
@@ -12,7 +12,7 @@ export default function Header() {
     return (
         <div className={`${hideSelf && 'hidden'} flex flex-row text-sand justify-center p-4`}>
             <Link target="_blank" className='flex font-host-grotesk text-[1.6rem]' href="/#fosdem">
-                <div className='flex'>AI Plumbers Devroom has been accepted for FOSDEM 2026! Submit your talk now before November 31st!</div>
+                <div className='flex'>AI Plumbers Devroom has been accepted for FOSDEM 2026! Make your plans for January 31!</div>
                 <div className='flex items-center'>
                     <div className='inline-block ml-[0.56vw] w-[1.53vw]'>
                         <ImageWrapper src={IMGHeaderIconArrowUpRight.src} alt="Arrow up right" />
